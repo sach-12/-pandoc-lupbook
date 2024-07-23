9a6f15f8de21dead965e59206b45ad014e322730
jh4yd1n3 <140603909+jh4yd1n3@users.noreply.github.com>
2024-07-23 04:12:01 -0700

    matching: implementation of labels

--- pandoc/matching_filter.py
+++ pandoc/matching_filter.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 LupLab
 # SPDX-License-Identifier: AGPL-3.0-only
 
-from dominate.tags import div
+from dominate.tags import div, span
 from dominate.util import raw
 
 import lupbook_filter
@@ -29,13 +29,14 @@ class LupbookMatching(lupbook_filter.LupbookComponent):
     def _activity_name(self):
         return "Matching activity"
 
-    def _gen_choice_block(self, choice):
+    def _gen_choice_block(self, choice, i):
         div_attrs = {
             "id": f"{self.prefix_id}-choice-{choice['id']}",
             "cls": "matching-choice bg-white border rounded m-2 mb-0 p-2 d-flex",
-            "data-match": choice['match']
+            "data-match": choice['match'],
         }
         with div(**div_attrs):
+            span(str(i + 1), cls = "badge text-bg-secondary me-2")
             text = choice['text']
             formatted_text = panflute.convert_text(text, output_format = 'html')
             raw(formatted_text)
@@ -64,8 +65,8 @@ class LupbookMatching(lupbook_filter.LupbookComponent):
                 with div(cls = "col"):
                     with div(id = f"{self.prefix_id}-choices",
                              cls = "matching-choices border pb-2 h-100"):
-                        for block in self.conf["choices"]:
-                            self._gen_choice_block(block)
+                        for i, block in enumerate(self.conf["choices"]):
+                            self._gen_choice_block(block, i)
                 with div(cls = "col"):
                     with div(id = f"{self.prefix_id}-answers",
                              cls = "border pb-2 h-100"):
@@ -75,9 +76,10 @@ class LupbookMatching(lupbook_filter.LupbookComponent):
     def _gen_testing_activity(self):
         div(id = f"{self.prefix_id}-testing-score",
             cls = "alert d-none")
-        for choice in self.conf["choices"]:
+        for i, choice in enumerate(self.conf["choices"]):
             formatted_text = panflute.convert_text(
                     choice["feedback"], output_format = 'html')
-            div(raw(formatted_text),
-                id = f"{self.prefix_id}-feedback-{choice['id']}",
-                cls = "matching-feedback-item m-1 p-2 border-start border-5 d-none")
+            with div(id = f"{self.prefix_id}-feedback-{choice['id']}",
+                     cls = "d-flex align-items-center matching-feedback-item m-1 p-2 border-start border-5 d-none"):
+                span(str(i + 1), cls="badge text-bg-secondary me-2")
+                div(raw(formatted_text))
