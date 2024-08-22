20f1a98917fe4918202d03c89a0ee9abe891cbfe
Joël Porquet-Lupine <joel@porquet.org>
2024-08-21 18:01:24 -0700

    sample: use codemirror from npm
    Signed-off-by: Joël Porquet-Lupine <joel@porquet.org>

--- sample/config.mk
+++
@@ -1,5 +0,0 @@
-# codemirror
-MODULE_DEPS += ext/codemirror/lib/codemirror.js
-MODULE_DEPS += ext/codemirror/lib/codemirror.css
-MODULE_DEPS += ext/codemirror/addon/edit/matchbrackets.js
-MODULE_DEPS += ext/codemirror/mode/clike/clike.js
