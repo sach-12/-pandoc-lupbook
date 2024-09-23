6d488a36b06bcd2f0632776c03d1100d1a9f0618
sach-12 <sachinshankar2010@gmail.com>
2024-09-22 21:48:21 -0700

    enhancement: refactors some code

--- modules/theme.js
+++ modules/theme.js
@@ -2,20 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
   const themeToggler = document.getElementById('theme-toggler');
   const htmlElement = document.documentElement;
 
+  const lightThemeHTML = '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>';
+  const darkThemeHTML = '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
+
   // Check and apply the user's theme preference stored in localStorage.
   // If not found, default to the system preference
   const storedTheme = localStorage.getItem('theme');
   if (storedTheme) {
     htmlElement.setAttribute('data-bs-theme', storedTheme);
     themeToggler.innerHTML = storedTheme === 'light' ?
-      '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>' :
-      '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
+      lightThemeHTML :
+      darkThemeHTML;
   } else {
     const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     htmlElement.setAttribute('data-bs-theme', systemTheme);
     themeToggler.innerHTML = systemTheme === 'light' ?
-      '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>' :
-      '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
+      lightThemeHTML :
+      darkThemeHTML;
   }
 
   // Toggle the theme when the user clicks the button
@@ -23,8 +26,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentTheme = htmlElement.getAttribute('data-bs-theme');
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
     themeToggler.innerHTML = newTheme === 'light' ?
-      '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>' :
-      '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
+      lightThemeHTML :
+      darkThemeHTML;
     htmlElement.setAttribute('data-bs-theme', newTheme);
     localStorage.setItem('theme', newTheme); // Store the user's preference
   });
