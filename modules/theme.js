f42e81b28e0214e318b9f341310defa9df2fec22
sach-12 <sachinshankar2010@gmail.com>
2024-09-22 21:43:54 -0700

    enhancement: implements a default mechanism to system preference if user has not stored their preference before

--- modules/theme.js
+++ modules/theme.js
@@ -2,11 +2,18 @@ document.addEventListener('DOMContentLoaded', function() {
   const themeToggler = document.getElementById('theme-toggler');
   const htmlElement = document.documentElement;
 
-  // Check and apply the user's theme preference stored in localStorage
+  // Check and apply the user's theme preference stored in localStorage.
+  // If not found, default to the system preference
   const storedTheme = localStorage.getItem('theme');
   if (storedTheme) {
     htmlElement.setAttribute('data-bs-theme', storedTheme);
     themeToggler.innerHTML = storedTheme === 'light' ?
+      '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>' :
+      '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
+  } else {
+    const systemTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    htmlElement.setAttribute('data-bs-theme', systemTheme);
+    themeToggler.innerHTML = systemTheme === 'light' ?
       '<i class="bi bi-moon"></i><span class="ms-2">Switch to dark mode</span>' :
       '<i class="bi bi-sun"></i><span class="ms-2">Switch to light mode</span>';
   }
